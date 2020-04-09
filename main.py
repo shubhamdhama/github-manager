6eeee9edae7b2da732f8ad18d84de100c0e6b18c
Shubham Dhama <shubhamdhamaofficial@gmail.com>
2020-04-09 19:38:22 +0530

    [Last year commit] Minor refactor.
    Change in wording of options.

--- main.py
+++ main.py
@@ -10,23 +10,17 @@ from utils.pulls import get_prs_info
 from utils.users import get_user_info
 
 
-TEMP_FILE = os.environ.get('TEMP_FILE')
+TEMP_FILE = os.environ.get('TEMP_FILE', "data/output.md")
 
 HELP_TEXT = """You can make following type of requests to GitHub v3 APIs (Enter the option number):
+(Please change the queries' code according to your use case first)
 
-MAKE SURE YOU'VE SPECIFIED `$TEMP_FILE` env variable
+(1) Get user information
 
-(1) Give a user information
-*No Auth required*
 (2) Get list of the PRs you've created
-Please change the query according to your use case
-*No Auth required*
+
 (3) Get the number of commits between a period.
-*No Auth required*
-
-Now `cat $TEMP_FILE`
 """
-
 
 def print_data(data: Dict[str, Any]) -> None:
     pp = pprint.PrettyPrinter(indent=4)
@@ -51,10 +45,14 @@ if __name__ == '__main__':
             "Enter the GitHub username of user whose GitHub public info you want: ")
         data = get_user_info(user)
         log_json_data(data)
+
     elif option == 2:
         data = get_prs_info()
         log_data(data)
+
     elif option == 3:
         data = get_commits_info()
-        print(len(data))
-        # log_json_data(data)
+        log_data(data)
+
+    else:
+        NotImplementedError()
